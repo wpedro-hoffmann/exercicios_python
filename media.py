1a360b2b727f70fca9a72df6ae80661f92172c78
wpedro-hoffmann <167827480+wpedro-hoffmann@users.noreply.github.com>
2024-04-23 19:58:57 -0300

    Create media.py

---
+++ media.py
@@ -0,0 +1,8 @@
+#Exercicios para resolver media de notas:
+
+nata_1 = float(input('Digite a nota 1:'))
+nota_2 = float(input('Digite a nota 2:'))
+
+media = (nota_1 + nota_2) / 2
+
+print('A nota final é:', media)
